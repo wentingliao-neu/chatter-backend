f5298c7d53831be0a3f65b6aeb4cd4e7206fbbbe
wentingliao-neu <liao.went@northeastern.edu>
2024-10-15 22:04:12 -0400

    changing capital

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -3,7 +3,7 @@ import { ChatsService } from './chats.service';
 import { ChatsResolver } from './chats.resolver';
 import { ChatsRepository } from './chats.repository';
 import { DatabaseModule } from 'src/common/database/database.module';
-import { Chat } from './entities/Chat.entity';
+import { Chat } from './entities/chat.entity';
 import { MessagesModule } from './messages/messages.module';
 import { ChatSchema } from './entities/chat.document';
 import { ChatsController } from './chats.controller';
--- src/chats/chats.repository.ts
+++ src/chats/chats.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Chat } from './entities/Chat.entity';
+import { Chat } from './entities/chat.entity';
 import { AbstractRepository } from 'src/common/database/abstract.repository';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
