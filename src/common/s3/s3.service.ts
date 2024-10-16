9260e7a993df3d4054cb3edabc68742c9e54e024
wentingliao-neu <liao.went@northeastern.edu>
2024-10-15 23:02:03 -0400

    changing environment variable

--- src/common/s3/s3.service.ts
+++ src/common/s3/s3.service.ts
@@ -7,7 +7,7 @@ import { FileUploadOptions } from './file-upload-options.interface';
 export class S3Service {
   private readonly client: S3Client;
   constructor(private readonly configService: ConfigService) {
-    const accessKeyId = configService.get('AWS_ACCESS_KEY');
+    const accessKeyId = configService.get('AWS_ACCESS_KEY_ID');
     const secretAccessKey = configService.get('AWS_SECRET_ACCESS_KEY');
     const region = configService.get('AWS_REGION');
     const clientConfig: S3ClientConfig = {};
